11f19060b8e33895a76dfda302694ddf12c9ae8b
Ubuntu <ubuntu@ip-172-31-16-111.eu-west-1.compute.internal>
2020-05-27 22:08:25 +0000

    dia 3

--- instalacion3/prepare-folders.sh
+++ instalacion3/prepare-folders.sh
@@ -7,7 +7,21 @@ mkdir -p /var/elasticsearch/data04
 mkdir -p /var/elasticsearch/data05
 mkdir -p /var/elasticsearch/data06
 mkdir -p /var/elasticsearch/data07
+mkdir -p /var/elasticsearch/data07
+mkdir -p /var/elasticsearch/data1
+mkdir -p /var/elasticsearch/data2
 
-chmod 777 -R /var/elasticsearch/data0*
+mkdir -p /var/elasticsearch/ingesta1
+mkdir -p /var/elasticsearch/ingesta2
+mkdir -p /var/elasticsearch/coordinador1
+mkdir -p /var/elasticsearch/maestro1
+mkdir -p /var/elasticsearch/maestro2
 
-sudo rm -rf /var/elasticsearch/data0*/*
+chmod 777 -R /var/elasticsearch/ingesta*
+chmod 777 -R /var/elasticsearch/coordinador*
+chmod 777 -R /var/elasticsearch/maestro*
+
+sudo rm -rf /var/elasticsearch/data*/*
+sudo rm -rf /var/elasticsearch/ingesta*/*
+sudo rm -rf /var/elasticsearch/coordinador*/*
+sudo rm -rf /var/elasticsearch/maestro*/*
